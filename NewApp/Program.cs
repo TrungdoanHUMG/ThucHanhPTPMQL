e78513d81a6fb00d4283623191b2c0617750e9e0
trungdoanhumg <2021050195@student.humg.edu.vn>
2023-09-07 17:38:18 +0700

    btapthuchanh

--- NewApp/Program.cs
+++ NewApp/Program.cs
@@ -1,2 +1,12 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿internal class Program 
+{
+    private static void Main(string[] args)
+    {
+         string hoTen = "Nguyen Trung Doan";
+         int Msv = 2021050195;
+         string Lop = " DTCTKTK65B";
+         int Khoa =65;
+        Console.WriteLine ("Sinh vien:{0} - Msv:{1}",hoTen,Msv);
+        Console.WriteLine ("Lop:{0} - K{1}",Lop,Khoa);
+    }
+}
