035354720a1ee769a3a52d44157033ee52c80d6c
trungdoanhumg <2021050195@student.humg.edu.vn>
2023-09-21 20:20:57 +0700

    creatclass

---
+++ NewApp/Models/Employee.cs
@@ -0,0 +1,28 @@
+
+namespace NewApp.Models
+{
+    public class Employee
+    {
+        public string MaNV { get; set; }
+        public string TenNV { get; set; }
+        public int Tuoi { get; set; }
+        public string Luong { get; set; }
+    
+        public void NhapTT(){
+                Console.Write("Ma NV = ");
+                MaNV = Console.ReadLine();
+                System.Console.Write("Ten NV = ");
+                TenNV = Console.ReadLine();
+                System.Console.Write("Tuoi = ");
+                Tuoi = Convert.ToInt16(Console.ReadLine());
+                System.Console.Write("luong = ");
+                Luong = Console.ReadLine();
+        }
+        public void HienTT(){
+            
+                System.Console.Write(MaNV + "-" + TenNV + "-" + Tuoi + "-" + Luong );
+            
+        }
+            
+    }
+}
--- NewApp/Program.cs
+++ NewApp/Program.cs
@@ -1,12 +1,5 @@
-﻿internal class Program 
-{
-    private static void Main(string[] args)
-    {
-         string hoTen = "Nguyen Trung Doan";
-         int Msv = 2021050195;
-         string Lop = " DTCTKTK65B";
-         int Khoa =65;
-        Console.WriteLine ("Sinh vien:{0} - Msv:{1}",hoTen,Msv);
-        Console.WriteLine ("Lop:{0} - K{1}",Lop,Khoa);
-    }
-}
+﻿using NewApp.Models;
+
+Employee emp = new Employee();
+emp.NhapTT();
+emp.HienTT();
