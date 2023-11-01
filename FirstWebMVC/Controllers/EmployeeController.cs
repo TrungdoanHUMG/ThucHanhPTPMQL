18620113d9ce88fde85db086b97706cb09d0955d
Trungdoan <2021050195@student.humg.edu.vn>
2023-11-02 00:13:30 +0700

    slide17

---
+++ FirstWebMVC/Controllers/EmployeeController.cs
@@ -0,0 +1,19 @@
+using Microsoft.AspNetCore.Mvc;
+
+namespace FirstWebMVC.Controllers ;
+
+public class EmployeeController : Controller
+{
+    public IActionResult Index()
+    {
+        return View();
+    }
+    [HttpPost]
+    public IActionResult Index(string fname, string email)
+    {
+        string strResult = "Xin chao : " + fname.ToUpper() + " - " + email;
+        ViewBag.Nhandulieu = strResult;
+        return View();
+    }
+
+}
